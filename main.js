9c4f67a903630e14f77d36cd0c5c7da63fc79ffb
anhkhoa55nn <123079882+anhkhoa55nn@users.noreply.github.com>
2024-02-07 19:23:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -66,6 +66,13 @@ const app = {
             app.chooseSong(VIE)
             currentPlaylist = VIE
         }
+        document.addEventListener('touchstart',function() {
+            app.render(VIE)
+            app.choose = true
+            cover.style.display = 'none'
+            app.chooseSong(VIE)
+            currentPlaylist = VIE
+        });
         document.querySelector('.type.USUK').onclick = function () {
             app.render(USUK)
             app.choose = true
